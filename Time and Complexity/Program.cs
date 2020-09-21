ac800102c7b3bc18c92583710d3808185434688d
AaqibGitBit <ahamedaaqib96@gmail.com>
2020-09-22 00:04:33 +0530

    Add modification

--- Time and Complexity/Program.cs
+++ Time and Complexity/Program.cs
@@ -6,7 +6,21 @@ namespace Time_and_Complexity
     {
         static void Main(string[] args)
         {
-            
-        }
+			int numberOfInputs = Convert.ToInt32(Console.ReadLine());
+			int i, j, input;
+			string output;
+			for (i = 1; i <= numberOfInputs; i++)
+			{
+				input = Convert.ToInt32(Console.ReadLine());
+				for (j = 2; j <= input / j; j++)
+				{
+					if (input % j == 0)
+						input = 1;
+				}
+				output = input == 1 ? "Not prime" : "Prime";
+				Console.WriteLine(output);
+			}
+
+		}
     }
 }
